47ef4ddbcfd72154c4c4e6d2dd7dc742e6100391
Tejas A Kumar <tejasakumar299@gmail.com>
2024-06-27 21:50:47 +0530

    perplexed

--- frontend/src/components/FileUpload.js
+++ frontend/src/components/FileUpload.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef,useEffect } from "react";
 import axios from "axios";
 import "./FileUpload.css";
 import Image from "./Image";
