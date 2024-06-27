153f12276890522cd021f6e16524ace12967e6f6
Tejas A Kumar <tejasakumar299@gmail.com>
2024-06-27 15:39:16 +0530

    websockets

--- Forensic_image_dump_handler.py
+++ Forensic_image_dump_handler.py
@@ -1,20 +1,26 @@
 from flask import *
 from flask_cors import CORS
+from flask_socketio import SocketIO, emit
+import time
 import BinProcessor
 import E01Processor
 app = Flask(__name__)
 CORS(app)
+socketio = SocketIO(app)
 
-@app.route("/" ,methods=["GET","POST"])
+def func():
+    with open("image.txt","rt") as f:
+        return f.read()
+@app.route("/", methods=["GET", "POST"])
 def receive_dump():
-    if request.method == "POST" :
+    if request.method == "POST":
         data = request.get_json()
         # print(data)
-        #[{name,content,extension}]
-        
+        # [{name,content,extension}]
+
         # print("data = ", data)
         if len(data):
-            array = data["result"] # type is a list
+            array = data["result"]  # type is a list
             # print("length of data = ", type(array))
             analyzer = None
             extension = array[0]["extension"]
@@ -25,14 +31,27 @@ def receive_dump():
                 # Add more extensions here
                 analyzer = BinProcessor.BinProcessor(array)
             print("suksuks")
-            analyzer.setupDump()
-            analyzer.startAnalysis()
+            # analyzer.setupDump()
+            # analyzer.startAnalysis()
             # analyzer.cleanUp()
-        
-        return "Successful",200
+            print("processing ....")
+            time.sleep(3)
+            processed_files = [
+                {"folder_name": "folder1",
+                 "number_of_files": 50,
+                 "File_array": [
+                     {
+                         "file_name": "file_name_1",
+                         "file_content": func()
+                     }
+                 ]},
+
+            ]
+        socketio.emit('file_processed', {
+                      'message': 'File has been processed successfully!', 'files': processed_files})
+    return jsonify({'status': 'File processed'}), 200
     return "not Handled request", 400
 
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0", debug=True)
